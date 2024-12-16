d7cd5b9661c01bf58a574765327f1a6dec6d3706
hayetmnasri <hayetmnasri@hotmail.fr>
2024-12-16 09:05:59 +0100

    UseRef exple5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import { useEffect } from 'react'
 import { useRef } from 'react'
+
 function App() {
-  const firstname=useRef()
-  const age=useRef()
+    const inputRef= useRef(null)
+    useEffect(()=>{
+        inputRef.current.focus()
+    },[])
   return (
     <div>
-      <input type='text' ref={firstname} /><br/>
-      <button onClick={()=>console.log(firstname.current.value)}>Print firstname</button><br/>
-      <input type='number' ref={age} /><br/>
-      <button onClick={()=>console.log(age.current.value)}>Print age</button>
+      <input type='text' ref={inputRef}/>
     </div>
   )
 }
